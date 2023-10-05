951322b606e5b207ae34d76ec8b7be1082051cc0
EirBec <eirbec.kodehode@gmail.com>
2023-10-05 08:41:18 +0200

    Hello world, node project with håkon

--- server.js
+++ server.js
@@ -1,104 +1,73 @@
 //imports
-const http = require("http");
 const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
+const express = require("express");
+const router = express.Router();
+const app = express();
+const cors = require("cors");
 
-const logEvents = require("./logEvents");
-const EventEmitter = require("events");
-const { log } = require("console");
-const { ht } = require("date-fns/locale");
 
-class TheEmitter extends EventEmitter {}
 
-const emitter = new TheEmitter();
+const {logger} = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 
-emitter.on("log", (msg, fileName) => logEvents(msg, fileName));
+const whitelist = ["http://localhost:3500", "http://localhost:127.0.0.1:5500"]
+const corsOptions = {
+    origin: (origin, callback) => 
+    {
+        if (whitelist.indexOf(origin) !== -1)
+        {
+            callback(null, true)
+        } 
+        else 
+        {
+            callback(new Error("You are not pure enough to access this site!"))
+        }
+    }, optionsSuccessStatus: 200
+}
+
+app.use(logger);
+app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  emitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-  const extension = path.extname(req.url);
+app.use(errorHandler)
 
-  let contentType;
-
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    default:
-      contentType = "text/html";
-      break;
-  }
-
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "view", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "view", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "view", req.url)
-      : path.join(__dirname, req.url);
-
-  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
-
-  const serveFile = async (filePath, contentType, res) => {
-    try {
-      const rawData = await fsPromises.readFile(
-        filePath,
-        !contentType.includes("image") ? "utf8" : ""
-      );
-      const data =
-        contentType === "application/json" ? JSON.parse(rawData) : rawData;
-      res.writeHead(filePath.includes("404.html") ? 404 : 200, {
-        "content-type": contentType,
-      });
-      res.end(contentType === "application/json" ? JSON.stringify(data) : data);
-    } catch (error) {
-      console.log(error);
-      emitter.emit("log", `${error.name}: ${error.message}`, "errorLog.txt");
-      res.statusCode = 500;
-      res.end();
-    }
-  };
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    serveFile(filePath, contentType, res);
-  } else {
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-        serveFile(path.join(__dirname, "view", "404.html"), "text/html", res);
-        break;
-    }
-  }
+app.get("^/$|index(.html)?", (req, res) => 
+{
+  res.sendFile(path.join(__dirname, "view", "index.html"));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/old-page(.html)?", (req, res) =>
+{
+    res.redirect(301, "/new-page.html");
+});
+
+app.get("*", (req, res) => 
+    {
+        res.status(404)
+        if(req.accepts("html"))
+        {
+            sendFile(path.join(__dirname, "view", "404.html"))
+        }
+        else if(req.accepts("json"))
+        {
+            res.json({error: "404 json not found"})
+        }
+        else (req.accepts("text"))
+        {
+            res.type("txt").send("404 text not found")
+        }
+
+        sendFile(path.join(__dirname, "view", "404.html"))
+    })
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "new-page.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
